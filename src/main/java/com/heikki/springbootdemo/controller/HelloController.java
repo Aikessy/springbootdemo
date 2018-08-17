03de2f6cb5ca11d3f58a5288e06aa7213a75a51e
hj <916310989@qq.com>
2018-08-17 17:35:15 +0800

    master conflict test

--- src/main/java/com/heikki/springbootdemo/controller/HelloController.java
+++ src/main/java/com/heikki/springbootdemo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello Spring Boot!";
+        return "Hello Springboot!";
     }
 
     @RequestMapping("/index")
