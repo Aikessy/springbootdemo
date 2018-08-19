26abc11b0cde1250fef74002a68e8d1e170e51a9
heikki <heikki606@163.com>
2018-08-19 11:17:53 +0800

    集成mybatis

---
+++ src/main/java/com/heikki/springbootdemo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.heikki.springbootdemo.controller;
+
+import com.heikki.springbootdemo.mapper.UserMapper;
+import com.heikki.springbootdemo.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    UserMapper userMapper;
+
+    @RequestMapping("/getAllUser")
+    @ResponseBody
+    public List<User> getAllUser(){
+        return userMapper.findAll();
+    }
+}
---
+++ src/main/java/com/heikki/springbootdemo/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.heikki.springbootdemo.mapper;
+
+import com.heikki.springbootdemo.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    @Select("select * from user")
+    List<User> findAll();
+}
